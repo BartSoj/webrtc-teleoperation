eb8b9c9b4fab67424d58805216e2b3a90324d8aa
Bartosz Sójka <bartsoj@gmail.com>
2024-07-23 11:23:17 +0200

    feat: add video capture with OpenCV and encoding to h.264
    (cherry picked from commit 02d180f609e90c34bac1e69af73cee0ad14a316d)

---
+++ pc_client/teleop_actions/StreamVideoCvAction.cpp
@@ -0,0 +1,110 @@
+#include "StreamVideoCvAction.hpp"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswscale/swscale.h>
+}
+
+void StreamVideoCvAction::init()
+{
+    cap.open(1);
+    if(!cap.isOpened())
+    {
+        std::cerr << "Error: Could not open camera" << std::endl;
+    }
+
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    // Initialize FFmpeg encoder
+    const AVCodec* codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    codecContext = avcodec_alloc_context3(codec);
+    codecContext->bit_rate = 400000;
+    codecContext->width = 640;
+    codecContext->height = 480;
+    codecContext->time_base = (AVRational){1, 30};
+    codecContext->gop_size = 10;
+    codecContext->max_b_frames = 0;
+    codecContext->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    if(avcodec_open2(codecContext, codec, nullptr) < 0)
+    {
+        std::cerr << "Error: Could not open codec" << std::endl;
+    }
+
+    avFrame = av_frame_alloc();
+    avFrame->format = codecContext->pix_fmt;
+    avFrame->width = codecContext->width;
+    avFrame->height = codecContext->height;
+    if(av_frame_get_buffer(avFrame, 32) < 0)
+    {
+        std::cerr << "Error: Could not allocate frame buffer" << std::endl;
+        return;
+    }
+
+    avPacket = av_packet_alloc();
+    if(!avPacket)
+    {
+        std::cerr << "Error: Could not allocate packet" << std::endl;
+        return;
+    }
+}
+
+bool StreamVideoCvAction::loop()
+{
+    cv::Mat frame;
+    cap >> frame;
+    if(frame.empty())
+    {
+        std::cerr << "Error: Could not capture frame" << std::endl;
+        cap.release();
+        return false;
+    }
+
+    // Convert frame to YUV420P
+    SwsContext* swsCtx =
+        sws_getContext(frame.cols, frame.rows, AV_PIX_FMT_BGR24, codecContext->width, codecContext->height,
+                       AV_PIX_FMT_YUV420P, SWS_BICUBIC, nullptr, nullptr, nullptr);
+    uint8_t* data[AV_NUM_DATA_POINTERS] = {frame.data, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+    int linesize[AV_NUM_DATA_POINTERS] = {static_cast<int>(frame.step), 0, 0, 0, 0, 0, 0, 0};
+    sws_scale(swsCtx, data, linesize, 0, frame.rows, avFrame->data, avFrame->linesize);
+    sws_freeContext(swsCtx);
+
+    // Encode frame
+    avFrame->pts = frameIndex++;
+
+    if(avcodec_send_frame(codecContext, avFrame) < 0)
+    {
+        std::cerr << "Error: Could not send frame for encoding" << std::endl;
+        return false;
+    }
+
+    while(avcodec_receive_packet(codecContext, avPacket) == 0)
+    {
+        avPacket->duration = av_rescale_q(1, codecContext->time_base, (AVRational){1, 30});
+
+        std::cout << "Packet info: PTS: " << avPacket->pts << ", DTS: " << avPacket->dts
+                  << ", Duration: " << avPacket->duration << ", Stream index: " << avPacket->stream_index << std::endl;
+
+        // Send encoded data via WebRTC
+        const std::byte* data = reinterpret_cast<const std::byte*>(avPacket->data);
+        size_t len = avPacket->size;
+
+        for(const auto& [id, peerConnection] : teleoperation->getPeerConnectionMap())
+        {
+            peerConnection->sendVideo(data, len);
+            std::cout << "Sent video to " << id << std::endl;
+        }
+
+        av_packet_unref(avPacket);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(33));
+
+    return true;
+}
---
+++ pc_client/teleop_actions/StreamVideoCvAction.hpp
@@ -0,0 +1,33 @@
+#ifndef LIBDATACHANNEL_APP_STREAMVIDEOCVACTION_H
+#define LIBDATACHANNEL_APP_STREAMVIDEOCVACTION_H
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswscale/swscale.h>
+}
+
+#include <opencv2/opencv.hpp>
+
+#include "Action.hpp"
+
+class StreamVideoCvAction : public Action
+{
+public:
+    using Action::Action;
+
+protected:
+    void init() override;
+
+    bool loop() override;
+
+    cv::VideoCapture cap;
+    int frameIndex;
+    AVCodecContext *codecContext;
+    AVFrame *avFrame;
+    AVPacket *avPacket;
+};
+
+#endif  // LIBDATACHANNEL_APP_STREAMVIDEOCVACTION_H
